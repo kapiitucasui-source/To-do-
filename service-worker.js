d0f15377a6327372969c8fce693994aa59d3599b
kapiitucasui-source <kapi.itucas.ui@phinmaed.com>
2026-03-20 15:06:51 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,22 @@
-self.addEventListener("install", function(e){
-  e.waitUntil("todo-cache".then(function(cache){
-    return cache.addAll(["index.html"]);
-  })
-);
+const CACHE_NAME = "todo-pwa-v1";
+const urlsToCache = [
+  "index.html",
+  "manifest.json",
+  "icon-192.png"
+];
+
+// Install
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
